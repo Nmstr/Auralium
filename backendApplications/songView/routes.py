ef30809661a22123371d57884ec344f8593796d7
namester <jonaskleinen674@gmail.com>
2024-01-17 16:40:22 +0100

    added img

--- backendApplications/songView/routes.py
+++ backendApplications/songView/routes.py
@@ -1,11 +1,10 @@
-from flask import Blueprint, render_template, session, redirect, url_for
+from flask import Blueprint, render_template, session, redirect, url_for, request
 
 songViewApp = Blueprint('songViewApp', __name__, template_folder='templates')
 
 @songViewApp.route('/songView/')
 def view():
     if 'loggedin' in session:
-        #song_name = request.args.get('song_name')
-        #return render_template('play.html', song_name=song_name, os=os)
-        return 'Hello World'
+        songName = request.args.get('songName')
+        return render_template('songView.html', songName=songName)
     return redirect(url_for('login'))
