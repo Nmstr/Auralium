ef3b130561f4d735188382889c41f94fecc25cd4
namester <jonaskleinen674@gmail.com>
2024-04-14 09:51:24 +0200

    resizable window and filter buttons

--- new/main.py
+++ new/main.py
@@ -10,7 +10,31 @@ class Main(QWidget):
         self.ui.homeBtn.clicked.connect(self.goHome)
         self.ui.searchBtn.clicked.connect(self.goSearch)
 
+        self.ui.searchFilterAllBtn.clicked.connect(self.switchSearchFilter)
+        self.ui.searchFilterSongsBtn.clicked.connect(self.switchSearchFilter)
+        self.ui.searchFilterArtistsBtn.clicked.connect(self.switchSearchFilter)
+
         self.show()
+    
+    def switchSearchFilter(self, filter):
+        """
+        This function handles the switching of search filters based on the senderName and the filter value.
+        """
+        senderName = self.sender().objectName()
+
+        if senderName == 'searchFilterAllBtn' and filter == True:
+            self.ui.searchFilterSongsBtn.setChecked(False)
+            self.ui.searchFilterArtistsBtn.setChecked(False)
+        elif senderName == 'searchFilterSongsBtn' and filter == True:
+            self.ui.searchFilterAllBtn.setChecked(False)
+            self.ui.searchFilterArtistsBtn.setChecked(False)
+        elif senderName == 'searchFilterArtistsBtn' and filter == True:
+            self.ui.searchFilterAllBtn.setChecked(False)
+            self.ui.searchFilterSongsBtn.setChecked(False)
+        else:
+            self.ui.searchFilterAllBtn.setChecked(True)
+            self.ui.searchFilterSongsBtn.setChecked(False)
+            self.ui.searchFilterArtistsBtn.setChecked(False)
     
     def goHome(self):
         self.ui.mainContentStack.setCurrentWidget(self.ui.home)
