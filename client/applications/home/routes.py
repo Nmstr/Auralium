694ea10160e3763662a7ed7c9993686280291bb6
namester <jonaskleinen674@gmail.com>
2024-04-14 16:17:30 +0200

    rmed old code

--- client/applications/home/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template, session, redirect, url_for
-
-homeApp = Blueprint('homeApp', __name__, template_folder='templates')
-
-@homeApp.route('/home/')
-def homeRoute():
-    return render_template('home.html')
--- client/deploy.sh
+++
@@ -1,5 +0,0 @@
-pip install python-dotenv
-pip install flask
-pip install mutagen
-pip install textdistance
-pip install eyed3
