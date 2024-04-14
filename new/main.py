d053c34f6137549c779d8970ffd83ba7cc923fbc
namester <jonaskleinen674@gmail.com>
2024-04-14 11:26:22 +0200

    most basic search poc

--- new/main.py
+++ new/main.py
@@ -1,6 +1,8 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QMessageBox
+from PyQt6.QtWidgets import QApplication, QWidget
 from PyQt6 import uic
+import difflib
 import sys
+import os
 
 class Main(QWidget):
     def __init__(self):
@@ -10,12 +12,31 @@ class Main(QWidget):
         self.ui.homeBtn.clicked.connect(self.goHome)
         self.ui.searchBtn.clicked.connect(self.goSearch)
 
+        self.ui.searchBar.textChanged.connect(self.searchBarTextChange)
         self.ui.searchFilterAllBtn.clicked.connect(self.switchSearchFilter)
         self.ui.searchFilterSongsBtn.clicked.connect(self.switchSearchFilter)
         self.ui.searchFilterArtistsBtn.clicked.connect(self.switchSearchFilter)
 
         self.show()
     
+    def searchBarTextChange(self, text):
+        """
+        This function handles the search functionality based on the search bar text.
+        """
+        print(text)
+        allSongs = os.listdir('music')
+        print(allSongs)
+        simmilar = difflib.get_close_matches(text, allSongs, n=3, cutoff=0.05)
+        print(simmilar)
+        if len(simmilar) < 3:
+            simmilar.append('')
+            simmilar.append('')
+            simmilar.append('')
+
+        self.ui.searchTopResult0Name.setText(simmilar[0])
+        self.ui.searchTopResult1Name.setText(simmilar[1])
+        self.ui.searchTopResult2Name.setText(simmilar[2])
+
     def switchSearchFilter(self, filter):
         """
         This function handles the switching of search filters based on the senderName and the filter value.
