df4093def145a40f228e5ed5336bcc9f7e0ca704
namester <jonaskleinen674@gmail.com>
2024-04-17 21:02:12 +0200

    fixed bugs that didnt show songs properly when searching for them

--- client/main.py
+++ client/main.py
@@ -89,7 +89,8 @@ class MainWindow(QWidget):
                 self.ui.musicControlsTime.setValue(0)
                 songQueue.goToNextSong()
         except Exception:
-            print('No song loaded')
+            #print('No song loaded')
+            pass
 
 
     def searchBarTextChange(self, text):
@@ -97,9 +98,11 @@ class MainWindow(QWidget):
         This function handles the search functionality based on the search bar text.
         """
         try:
-            allSongs = sqlHandler.retrieveAllSongTitles()
+            #allSongs = sqlHandler.retrieveAllSongTitles()
+            allSongs = sqlHandler.retrieveAllSongs()[1:4]
             simmilar = difflib.get_close_matches(text, allSongs, n=3, cutoff=0.05)
             simmilar = simmilar + [sqlHandler.retrieveRandomSong()[1:4] for _ in range(3 - len(simmilar))]
+            print(simmilar)
         except Exception:
             simmilar = ['No results', 'No results', 'No results']
         # Update top results labels
