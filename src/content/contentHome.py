bed6d5768d4421a3efde4a678b1590241e636026
Nmstr <jonaskleinen325@gmail.com>
2024-06-07 20:42:19 +0200

    removed old unused debug buttons in home

--- src/content/contentHome.py
+++ src/content/contentHome.py
@@ -9,10 +9,3 @@ class ContentHomeWidget(BaseClass, UiContentHome):
 
         super().__init__()
         self.setupUi(self)
-
-        # Connect playlist buttons
-        self.playlistsRetrieveBtn.clicked.connect(lambda: print(self.mainWindow.sqlHandler.playlists.retrieve(1)))
-        self.playlistsRetrieveAllBtn.clicked.connect(lambda: print(self.mainWindow.sqlHandler.playlists.retrieveAll()))
-        self.playlistsAddSongBtn.clicked.connect(lambda: self.mainWindow.sqlHandler.playlists.addSong(1, 500, 999999))
-        self.playlistsRemoveSongBtn.clicked.connect(lambda: self.mainWindow.sqlHandler.playlists.removeSong(1, 3))
-        self.playlistsMoveSongBtn.clicked.connect(lambda: self.mainWindow.sqlHandler.playlists.moveSong(1, 3, 1))
